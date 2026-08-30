040a6b2348de66192f7fb91a77be310adeb8ba4d
DDairy <26867342+bradleytenuta@users.noreply.github.com>
2026-08-30 10:07:06 +0100

    Updated the position of the 3d model and removed some mapbox ui features like compase and scalebar.

--- app/src/main/java/com/bradleytenuta/stoutandabout/pages/map/MapScreen.kt
+++ app/src/main/java/com/bradleytenuta/stoutandabout/pages/map/MapScreen.kt
@@ -1,8 +1,10 @@
 package com.bradleytenuta.stoutandabout.pages.map
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.mapbox.maps.MapboxExperimental
 import com.mapbox.maps.Style
 import com.mapbox.maps.extension.compose.MapEffect
@@ -23,6 +25,14 @@ class MapScreen {
         MapboxMap(
             modifier = Modifier.fillMaxSize(),
             mapViewportState = mapViewportState,
+            compass = {},
+            scaleBar = {},
+            logo = {
+                Logo(contentPadding = PaddingValues(start = 12.dp, bottom = 36.dp))
+            },
+            attribution = {
+                Attribution(contentPadding = PaddingValues(start = 100.dp, bottom = 36.dp))
+            }
         ) {
             MapEffect(Unit) { mapView ->
                 mapView.mapboxMap.loadStyle(Style.MAPBOX_STREETS)
@@ -32,7 +42,9 @@ class MapScreen {
                     puckBearing = PuckBearing.HEADING
                     locationPuck = LocationPuck3D(
                         modelUri = "asset://casual_character.glb",
-                        modelScale = listOf(60f, 60f, 60f)
+                        modelScale = listOf(60f, 60f, 60f),
+                        // Adjust the 3rd value (90f) if the character faces the wrong direction
+                        modelRotation = listOf(0f, 0f, 180f)
                     )
                 }
                 mapView.gestures.updateSettings {
